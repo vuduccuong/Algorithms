b6316f961104747808f8f528bf0800aa73de93ac
Vũ Cường <phantom.yi@hotmail.com>
2019-02-11 09:30:57 +0700

    Regular Expression

---
+++ Regular Expression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regular_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"\d");
+            foreach (Match item in reg.Matches("-vuduccuong-18061996"))
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
