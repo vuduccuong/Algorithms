5bc85bb09f33dbd48eb4b414d3ef029c87a7e749
Vũ Cường <phantom.yi@hotmail.com>
2019-02-19 09:36:52 +0700

    Tìm giá trị chênh lệch lớn nhất của 2 phần tử cạnh nhau trong mảng

---
+++ Array Maximal Adjacent Difference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Maximal_Adjacent_Difference
+{
+    class Program
+    {
+        /*
+         * Tìm giá trị chênh lệch lớn nhất của 2 phần tử cạnh nhau trong mảng
+         * Input: [1,2,5,4,20]
+         * Output:16 (20-4)
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] arr = new[] {1, 2, 3, 5, 4, 20};
+            Console.WriteLine("mảng đầu vào: ");
+            foreach (var VARIABLE in arr)
+            {
+                Console.Write(VARIABLE);
+            }
+
+            Console.WriteLine("\nĐộ chênh lệch giá trị lớn nhất là : {0}", ArrayMaximalAdjacentDifference(arr));
+            Console.ReadLine();
+        }
+        static int ArrayMaximalAdjacentDifference(int[] inputArray)
+        {
+            int countDiffenren = 0;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+
+                countDiffenren = Math.Max(Math.Abs(inputArray[i] - inputArray[i + 1]), countDiffenren);
+            }
+            return countDiffenren;
+        }
+
+    }
+}
--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace palindrome
 {
-    class Program
+    internal class Program
     {
         /*
          * Kiểm tra một chuỗi nhập vào có sắp xếp được thành chuỗi đối xứng hay không
@@ -14,7 +12,8 @@ namespace palindrome
          * Input: "aabbc
          * Output: true ("abcba")
          */
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.WriteLine("Nhập chuỗi cần kiểm tra: ");
@@ -23,29 +22,52 @@ namespace palindrome
             Console.WriteLine("is Palindrom? : {0}", check);
             Console.ReadLine();
         }
-
-        private static bool PalindromeRearranging(string s)
+        static int NO_OF_CHARS = 256;
+        private static bool PalindromeRearranging(string str)
         {
-            s = String.Concat(s.OrderBy(c => c));
-            if (s.Length == 1) return true;
-            for (int i = s.Length; i >0; i--)
-            {
-                if (s[0] == s[1])
-                {
-                    s = s.Remove(0, 2);
-                    
-                    if (s.Length < 1)
-                    {
-                        return true;
-                    }
+            //s = String.Concat(s.OrderBy(c => c));
+            //if (s.Length == 1) return true;
+            //for (int i = s.Length; i > 0; i--)
+            //{
+            //    if (s[0] == s[1])
+            //    {
+            //        s = s.Remove(0, 2);
 
-                    Console.WriteLine(s);
-                    Console.WriteLine(s.Length);
-                }
+            //        if (s.Length < 1)
+            //        {
+            //            return true;
+            //        }
+
+            //        Console.WriteLine(s);
+            //        Console.WriteLine(s.Length);
+            //    }
+            //}
+
+            //return false;
+
+            
+            int[] count = new int[NO_OF_CHARS];
+            count = count.Select(i => 0).ToArray();
+
+            // For each character in input strings, 
+            // increment count in the corresponding 
+            // count array 
+            for (int i = 0; i < str.Length; i++)
+                count[(int)(str[i])]++;
+
+            // Count odd occurring characters 
+            int odd = 0;
+            for (int i = 0; i < NO_OF_CHARS; i++)
+            {
+                if ((count[i] & 1) == 1)
+                    odd++;
+
+                if (odd > 1)
+                    return false;
             }
 
-            return false;
-
+            // Return true if odd count is 0 or 1, 
+            return true;
         }
     }
 }
